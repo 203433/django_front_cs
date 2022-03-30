bb9e0ee8360ec49b9d3c730be33202648a6117d5
203433 <203433@ids.upchiapas.edu.mx>
2022-03-30 13:00:13 -0600

    Fixes in some bugs and smell code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter, Route, Routes, Navigate} from 'react-router-dom';
 import Register from "./Components/Register/Register";
